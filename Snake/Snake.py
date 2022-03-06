ad635946aae5f8bc010321a2633189bdccfaef24
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-03-06 17:20:43 +0900

    Random position for each new box

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -53,7 +53,21 @@ class snake(object):
                 self.dirny = 1
                 self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-        
+        # Random movement of new cube
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn [0], turn[1])
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)  
+
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                else: c.move(c.dirnx,c.dirny)
 
 # Creating grid
 def drawGrid(w, rows,surface):
