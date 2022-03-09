bb384ff5609f58c3e00a0fa8628fa9ec6a2b1042
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-03-09 16:41:19 +0900

    Failure movement

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -2,29 +2,31 @@
 import math
 import random
 import pygame 
+import tkinter as tk
+from tkinter import messagebox
 
-class cube(object):
+class dot(object):
     rows = 20
-    w = 500
+    width = 500
 
-    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+    def __init__(self, start, dirx=1, diry=0, color=(255,0,0)):
         self.pos = start
-        self.dirnx = 1
-        self.dirny = 0
+        self.dirx = 1
+        self.diry = 0
         self.color = color
 
-    def move(self, dirnx, dirny):
-        self.dirnx = dirnx
-        self.dirny = dirny
-        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+    def move(self, dirx, diry):
+        self.dirx = dirx
+        self.diry = diry
+        self.pos = (self.pos[0] + self.dirx, self.pos[1] + self.diry)
 
-    def draw(self, surface, eyes=False):
-        dis = self.w // self.rows
+    def draw(self, surface, head=False):
+        dis = self.width // self.rows
         i = self.pos[0]
         j = self.pos[1]
  
         pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
-        if eyes:
+        if head:
             centre = dis//2
             radius = 3
             circleMiddle = (i*dis+centre-radius,j*dis+8)
@@ -38,10 +40,10 @@ class snake(object):
 
     def __init__(self, color, pos):
         self.color = color
-        self.head = cube(pos)
+        self.head = dot(pos)
         self.body.append(self.head)
-        self.dirnx = 0
-        self.dirny = 1
+        self.dirx = 0
+        self.diry = 1
 
     def movement(self):
         for event in pygame.event.get():
@@ -52,26 +54,26 @@ class snake(object):
 
         for key in keys:
             if keys [pygame.K_LEFT] :
-                self.dirnx = -1
-                self.dirny = 0
-                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]        
+                self.dirx = -1
+                self.diry = 0
+                self.turns[self.head.pos[:]] = [self.dirx, self.diry]        
 
             elif keys [pygame.K_RIGHT] :
-                self.dirnx = 1
-                self.dirny = 0
-                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]    
+                self.dirx = 1
+                self.diry = 0
+                self.turns[self.head.pos[:]] = [self.dirx, self.diry]    
 
             elif keys [pygame.K_UP] :
-                self.dirnx = 0
-                self.dirny = -1
-                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                self.dirx = 0
+                self.diry = -1
+                self.turns[self.head.pos[:]] = [self.dirx, self.diry]
 
             elif keys [pygame.K_DOWN] :
-                self.dirnx = 0
-                self.dirny = 1
-                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                self.dirx = 0
+                self.diry = 1
+                self.turns[self.head.pos[:]] = [self.dirx, self.diry]
 
-        # Random movement of new cube
+        # Random movement of new dot
         for i, c in enumerate(self.body):
             p = c.pos[:]
             if p in self.turns:
@@ -81,30 +83,35 @@ class snake(object):
                     self.turns.pop(p)  
 
             else:
-                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
-                else: c.move(c.dirnx,c.dirny)
+                if c.dirx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.diry == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.diry == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                else: c.move(c.dirx,c.diry)
 
     def reset(self, pos):
-        pass
+        self.head = dot(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirx = 0
+        self.diry = 1
 
-    def addCube(self):
+    def adddot(self):
         tail = self.body[-1]
-        dx, dy = tail.dirnx, tail.dirny
+        dx, dy = tail.dirx, tail.diry
  
         if dx == 1 and dy == 0:
-            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
+            self.body.append(dot((tail.pos[0]-1,tail.pos[1])))
         elif dx == -1 and dy == 0:
-            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
+            self.body.append(dot((tail.pos[0]+1,tail.pos[1])))
         elif dx == 0 and dy == 1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
+            self.body.append(dot((tail.pos[0],tail.pos[1]-1)))
         elif dx == 0 and dy == -1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
+            self.body.append(dot((tail.pos[0],tail.pos[1]+1)))
  
-        self.body[-1].dirnx = dx
-        self.body[-1].dirny = dy
+        self.body[-1].dirx = dx
+        self.body[-1].diry = dy
 
     def draw(self, surface):
         for i, c in enumerate(self.body):
@@ -114,8 +121,8 @@ class snake(object):
                 c.draw(surface)
 
 # Creating grid
-def drawGrid(w, rows,surface):
-    sizeBtwn = w // rows
+def drawGrid(width, rows,surface):
+    sizeBtwn = width // rows
 
     x = 0
     y = 0
@@ -124,19 +131,19 @@ def drawGrid(w, rows,surface):
         x = x + sizeBtwn
         y = y + sizeBtwn
 
-        pygame.draw.line(surface, (255,255,255), (x, 0), (x, w))
-        pygame.draw.line(surface, (255,255,255), (0, y), (w, y))
+        pygame.draw.line(surface, (255,255,255), (x, 0), (x, width))
+        pygame.draw.line(surface, (255,255,255), (0, y), (width, y))
 
 # Updating grid
 def redrawWindow(surface):
-    global rows, width, s, snack
+    global rows, width, snake_object, newDot
     surface.fill((0,0,0))
-    s.draw(surface)
-    snack.draw(surface)
+    snake_object.draw(surface)
+    newDot.draw(surface)
     drawGrid(width, rows, surface)
     pygame.display.update()
 
-def randomSnack(rows, items):
+def randomnewDot(rows, items):
     positions = items.body
 
     while True:
@@ -149,15 +156,25 @@ def randomSnack(rows, items):
 
     return (x,y)    
 
+def message_box(subject, content):
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
+
 # Main loop of the game
 def main():
-    global width, rows, s, snack
+    global width, rows, snake_object, newDot
     width = 500
     rows = 20
 
     win = pygame.display.set_mode((width, width)) # Defining display
-    s = snake((255, 0, 0), (10, 10)) # Creating snake object
-    snack = cube(randomSnack(rows, s), color = (0, 255, 0))
+    snake_object = snake((255, 0, 0), (10, 10)) # Creating snake object
+    newDot = dot(randomnewDot(rows, snake_object), color = (0, 255, 0))
     flag = True 
 
     clock_game = pygame.time.Clock() # Defining variable related to the speed of the movement
@@ -166,10 +183,18 @@ def main():
         # Controlling the speed of movement
         pygame.time.delay(50)
         clock_game.tick(10)
-        s.move()
-        if s.body[0].pos == snack.pos:
-            s.addCube()
-            snack = cube(randomSnack(rows, s), color = (0, 255, 0))
+        snake_object.move()
+        if snake_object.body[0].pos == newDot.pos:
+            snake_object.adddot()
+            newDot = dot(randomnewDot(rows, snake_object), color = (0, 255, 0))
+
+        for x in range(len(snake_object.body)):
+            if snake_object.body[x].pos in list(map(lambda z:z.pos,snake_object.body[x+1:])):
+                print('Score: ', len(snake_object.body))
+                message_box('You Lost!', 'Play again')
+                snake_object.reset((10,10))
+                break
+
         redrawWindow (win)
 
     pass    
