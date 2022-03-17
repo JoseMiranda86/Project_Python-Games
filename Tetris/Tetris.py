195184a7d5d3d1de01f054072915d7d32b9d3a01
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-03-17 10:36:31 +0900

    Function for new shapes

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -218,3 +218,37 @@ def clear_rows(grid, locked):
             if y < ind:
                 newKey = (x, y + inc)
                 locked[newKey] = locked.pop(key)
+
+def draw_next_shape(shape, surface):
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Next Shape', 1, (255,255,255))
+
+    sx = top_left_x + playing_area_width + 50
+    sy = top_left_y + playing_area_height/2 - 100
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                pygame.draw.rect(surface, shape.color, (sx + j*30, sy + i*30, 30, 30), 0)
+
+    surface.blit(label, (sx + 10, sy- 30))
+
+
+def draw_window(surface):
+    surface.fill((0,0,0))
+    font = pygame.font.SysFont('comicsans', 60)
+    label = font.render('TETRIS', 1, (255,255,255))
+
+    surface.blit(label, (top_left_x + playing_area_width / 2 - (label.get_width() / 2), 30))
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j* 30, top_left_y + i * 30, 30, 30), 0)
+
+    draw_grid(surface, 20, 10)
+    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, playing_area_width, playing_area_height), 5)
+
+def main():
+    global grid
