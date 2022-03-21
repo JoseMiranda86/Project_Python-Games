bce05010b8f447e4aa9e89f676dd6817dc70b16b
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-03-21 16:03:55 +0900

    Main function on going

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -276,3 +276,37 @@ def main():
             if not (empty_space(current_piece, grid)) and current_piece.y > 0:
                 current_piece.y -= 1
                 change_piece = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                    if not empty_space(current_piece, grid):
+                        current_piece.x += 1
+
+                elif event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                    if not empty_space(current_piece, grid):
+                        current_piece.x -= 1
+                elif event.key == pygame.K_UP:
+                    # rotate shape
+                    current_piece.rotation = current_piece.rotation + 1 % len(current_piece.shape)
+                    if not empty_space(current_piece, grid):
+                        current_piece.rotation = current_piece.rotation - 1 % len(current_piece.shape)
+
+                if event.key == pygame.K_DOWN:
+                    # move shape down
+                    current_piece.y += 1
+                    if not empty_space(current_piece, grid):
+                        current_piece.y -= 1
+
+                if event.key == pygame.K_SPACE:
+                   while empty_space(current_piece, grid):
+                       current_piece.y += 1
+                   current_piece.y -= 1
+                   print(convert_shape_format(current_piece))
