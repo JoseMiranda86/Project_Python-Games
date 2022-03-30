e43fd59ca0052f214bf837a83e2c4a24d4649fa5
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-03-30 21:47:24 +0900

    Definition variables

--- Connect4/Connect4.py
+++ Connect4/Connect4.py
@@ -63,3 +63,26 @@ def draw_board(board):
 			elif board[r][c] == 2:
 				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
 	pygame.display.update()
+
+board = create_board()
+print_board(board)
+game_over = False
+turn = 0
+
+pygame.init()
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE/2 - 5)
+
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("monospace", 75)
+
