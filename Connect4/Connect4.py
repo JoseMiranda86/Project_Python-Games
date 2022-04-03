ba07c42fb59605b0e6f18218726f630807caf2e7
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-04-03 21:10:40 +0900

    Mouse mov

--- Connect4/Connect4.py
+++ Connect4/Connect4.py
@@ -102,3 +102,21 @@ while not game_over:
 			else:
 				pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
 		pygame.display.update()
+
+		
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+			#print(event.pos)
+			# Ask for Player 1 Input
+			if turn == 0:
+				posx = event.pos[0]
+				col = int(math.floor(posx/SQUARESIZE))
+
+				if is_valid_location(board, col):
+					row = get_next_open_row(board, col)
+					drop_piece(board, row, col, 1)
+
+					if winning_move(board, 1):
+						label = myfont.render("Player 1 wins!!", 1, RED)
+						screen.blit(label, (40,10))
+						game_over = True
