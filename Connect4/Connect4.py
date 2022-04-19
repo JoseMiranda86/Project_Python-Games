7a7a8110deafb618f15207d9641d8bb005f3618c
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-04-19 21:40:49 +0900

    Creating For loop for the game event

--- Connect4/Connect4.py
+++ Connect4/Connect4.py
@@ -140,3 +140,16 @@ while not game_over:
 
 			if game_over:
 				pygame.time.wait(3000)
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+
+		if event.type == pygame.MOUSEMOTION:
+			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+			posx = event.pos[0]
+			if turn == 0:
+				pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+			else:
+				pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+		pygame.display.update()				
